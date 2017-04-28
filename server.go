8fa4a75dfb5575bc96211dd6dcae817685cadc4d
harshita14 <lalwaniharshita104@gmail.com>
2017-04-28 15:46:53 +0530

    search for duckduckgo and google  implemented

--- server.go
+++ server.go
@@ -8,7 +8,8 @@ import(
     "strings"
 )
 func main() {
-	fmt.Println(duckDuckGoSearch("dog"))
+	//fmt.Println(duckDuckGoSearch("sweets"))
+	fmt.Println(googleSearch("the dark knight"))
 }
 
 func duckDuckGoSearch(query string) string{
@@ -38,5 +39,34 @@ func duckDuckGoSearch(query string) string{
     //8 is added to remove the "Text": part
     result := bodyString[posFirst+8:]             
     posLast := strings.Index(result, "\"}")
+    return result[:posLast]
+}
+
+func googleSearch(query string) string{
+	timeout := time.Duration(time.Second*5)  //timeout set to one second
+	client := http.Client{
+    	Timeout: timeout,
+	}
+    // Make a get request
+	s := []string{"https://www.googleapis.com/customsearch/v1?key=AIzaSyBwkNbaIKhYBqghBwxsNH1ES4Ze_4WphPk&cx=017576662512468239146:omuauf_lfve&q=", query}
+	queryString := strings.Join(s, "")
+    rs, err := client.Get(queryString)
+    // Process response
+    if err != nil {
+        panic(err) 
+    }
+    //close the response at the end of the function
+    defer rs.Body.Close()  
+    bodyBytes, err := ioutil.ReadAll(rs.Body)
+    if err != nil {
+        panic(err)
+    }
+    //bodyString is the json response as a string
+    bodyString := string(bodyBytes)
+    //slicing bodystring for getting first result
+    posFirst := strings.Index(bodyString, "\"title\":")
+    //8 is added to remove the "Text": part
+    result := bodyString[posFirst+10:]             
+    posLast := strings.Index(result, "\",")
     return result[:posLast]
 }
